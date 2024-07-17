1e2f202ab3810b5c97fe46055a5fd9e7f57e75b2
evelynmitchell <efmphone@gmail.com>
2024-07-17 12:36:47 -0600

    get stock details

--- main.py
+++ main.py
@@ -12,3 +12,16 @@ with st.sidebar:
 	# Authenticate with the Polygon API
     client = RESTClient(polygon_api_key)
     col1, col2, col3 = st.columns(3)
+
+ #Stock details
+if col1.button("Get Details"):
+ if not polygon_api_key.strip() or not symbol.strip():
+        st.error("Please add your Polygon API Key on the left <--")
+    else:
+        try:
+            details = client.get_ticker_details(symbol)
+            st.success(f"Ticker: {details.ticker}\\n\\n"
+                f"Company Address: {details.address}\\n\\n"
+                f"Market Cap: {details.market_cap}")
+        except Exception as e:
+            st.exception(f"Exception: {e}") 
