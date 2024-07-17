1ff90766b44102bfc1c0da069c9f18b626936069
evelynmitchell <efmphone@gmail.com>
2024-07-17 12:44:30 -0600

    add chart

--- main.py
+++ main.py
@@ -42,3 +42,26 @@ if col2.button("Get Quote"):
                     f"Volume: {agg.volume}")
         except Exception as e:
             st.exception(f"Exception: {e}")
+
+#historical data for the year
+if col3.button("Get Historical"):
+    if not polygon_api_key.strip() or not symbol.strip():
+        st.error("Please add your Polygon API Key on the left <--")
+    else:
+        try:
+            dataRequest = client.list_aggs(
+                ticker = symbol,
+                multiplier = 1,
+                timespan = "day",
+                from_ = "2024-01-01",
+                to = "2024-04-29"
+            )
+            chart_data = pd.DataFrame(dataRequest)
+    
+            chart_data['date_formatted'] = chart_data['timestamp'].apply(
+                          lambda x: pd.to_datetime(x*1000000))
+      
+            st.line_chart(chart_data, x="date_formatted",y="close")
+    
+        except Exception as e:
+            st.exception(f"Exception: {e}") 
